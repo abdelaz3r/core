494d5531071e3a24ab553240e4f4393e9cf5ed98
abdelaz3r <gil.clavien@gmail.com>
2022-07-08 16:17:36 +0200

    Add porcelain dependency

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Core.MixProject do
   def application do
     [
       mod: {Core.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :porcelain]
     ]
   end
 
@@ -48,7 +48,8 @@ defmodule Core.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:porcelain, "~> 2.0"}
     ]
   end
 
