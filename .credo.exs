9f585f7c26f23dd070f6ec4594e4e2fcfc82d435
abdelaz3r <gil.clavien@gmail.com>
2022-09-15 15:35:25 +0200

    Update credo rules

--- .credo.exs
+++ .credo.exs
@@ -67,7 +67,7 @@
           {Credo.Check.Readability.Semicolons, []},
           {Credo.Check.Readability.SeparateAliasRequire, []},
           {Credo.Check.Readability.SingleFunctionToBlockPipe, []},
-          {Credo.Check.Readability.SinglePipe, []},
+          {Credo.Check.Readability.SinglePipe, false},
           {Credo.Check.Readability.SpaceAfterCommas, []},
           {Credo.Check.Readability.Specs, false},
           {Credo.Check.Readability.StrictModuleLayout, []},
